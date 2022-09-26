ef8e84aeeb5dbdbdfbaacf2576e0ddf6485f44ec
JuanIsa <JuanIsa>
2022-09-26 09:48:17 -0300

    1.0.2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import io from 'socket.io-client';
 import { useState, useEffect } from 'react';
-const socket = io('https://server-push-production.up.railway.app/');
+//https://server-push-production.up.railway.app/
+const socket = io('http://localhost:4000');
 
 function App() {
   const [mensaje, setMensaje] = useState('');
+  const [sala, setSala] = useState('');
   const [mensajes, setMensajes] = useState([]);
+  const [flag, setFlag] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
-    socket.emit('mensajeDesdeFront', mensaje);
-    setMensaje('');
+    socket.emit('subscribe', sala);
+    setFlag(true)
+  }
+  function handleSubmit2(e) {
+    e.preventDefault();
+    socket.emit('subscribe', sala);
+    socket.emit('send message', mensaje);
+    
     setMensajes([...mensajes, {
       usuario: 'Yo',
       contenido: mensaje
     }]);
+    setMensaje('');
   }
+ 
   useEffect(() => {
-    socket.on('mensajedelBak', value => {
+    socket.on('back', value => {
       setMensajes([...mensajes, { ...value }])
     });
   }, [mensajes]);
+
   return (
     <div id='principal'>
       <h1>Chat en vivo</h1>
-      Programador Juan Isa
+        
       <form onSubmit={handleSubmit}>
+        <label>
+          Key secreta;
+          <input type="text" onChange={e => setSala(e.target.value)} value={sala} />
+        </label>
+        <button >Enviar</button>
+      </form>
+      {flag > 0 && <h3>Chat secreto:{sala}</h3>}
+      Programador Juan Isa
+      <form onSubmit={handleSubmit2}>
         <input type="text" onChange={e => setMensaje(e.target.value)} value={mensaje} />
         <button >Enviar</button>
       </form>
